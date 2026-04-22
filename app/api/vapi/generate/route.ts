bb17c94887ce760f47dc9ea8ed2f77874a5b3a84
zohaib4991 <zohaibmunir528@gmail.com>
2026-04-22 09:19:32 +0500

    switch to gemini-1.5-flash

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -9,7 +9,9 @@ export async function POST(request: Request) {
 
     try {
         const { text: questions } = await generateText({
-            model: google("gemini-2.0-flash-001"),
+
+            // model: google("gemini-2.0-flash-001"),
+            model: google("gemini-1.5-flash"),
             prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
