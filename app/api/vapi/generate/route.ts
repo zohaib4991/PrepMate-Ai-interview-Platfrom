42c925ef14d69871620f153dcc2a4a606fdb7a1f
zohaib4991 <zohaibmunir528@gmail.com>
2026-04-22 11:09:00 +0500

    using latest gemini model

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
     try {
         const { text: questions } = await generateText({
 
-            model: google("gemini-2.0-flash-001"),
+            model: google("gemini-2.0-flash"),
             prompt: `Prepare questions for a job interview.
         The job role is ${role}.
         The job experience level is ${level}.
